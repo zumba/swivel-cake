42833c8463618e51a74e4419ef99c134ee588e8a
Stephen Young <young.steveo@gmail.com>
2015-04-27 15:57:38 -0400

    Added ability to set the bucket index after configuration

--- Lib/SwivelLoader.php
+++ Lib/SwivelLoader.php
@@ -5,6 +5,13 @@ App::uses('ClassRegistry', 'Utility');
 class SwivelLoader {
 
 	/**
+	 * Swivel config
+	 *
+	 * @var \Zumba\Swivel\Config
+	 */
+	protected $config;
+
+	/**
 	 * Configuration options
 	 *
 	 * @var array
@@ -25,6 +32,26 @@ class SwivelLoader {
 	 */
 	public function __construct($options) {
 		$this->options = $options;
+	}
+
+	/**
+	 * Get the swivel config instance
+	 *
+	 * @return \Zumba\Swivel\Config
+	 */
+	public function getConfig() {
+		if (empty($this->config)) {
+			$options = $this->options;
+			$this->config = new \Zumba\Swivel\Config(
+				$this->getModel()->getMapData(),
+				$options['BucketIndex'],
+				$options['Logger']
+			);
+			if (!empty($options['Metrics'])) {
+				$config->setMetrics($options['Metrics']);
+			}
+		}
+		return $this->config;
 	}
 
 	/**
@@ -53,17 +80,27 @@ class SwivelLoader {
 	 * @return \Zumba\Swivel\Manager
 	 */
 	protected function load() {
-		$options = $this->options;
-		$config = new \Zumba\Swivel\Config(
-			$this->getModel()->getMapData(),
-			$options['BucketIndex'],
-			$options['Logger']
-		);
-		if (!empty($options['Metrics'])) {
-			$config->setMetrics($options['Metrics']);
-		}
-		$this->manager = new \Zumba\Swivel\Manager($config);
-
+		$this->manager = new \Zumba\Swivel\Manager($this->getConfig());
 		return $this->manager;
+	}
+
+	/**
+	 * Used to set the bucket index before loading swivel.
+	 *
+	 * @param integer $index Number between 1 and 10
+	 * @return void
+	 * @throws InvalidArgumentException if $index is not valid
+	 */
+	public function setBucketIndex($index) {
+		if (!is_numeric($index) || $index < 1 || $index > 10) {
+			throw new InvalidArgumentException("$index is not a valid bucket index.");
+		}
+		if (empty($this->manager)) {
+			$this->options['BucketIndex'] = $index;
+		} else {
+			$config = $this->getConfig();
+			$config->setBucketIndex($index);
+			$this->manager->setBucket($config->getBucket());
+		}
 	}
 }
